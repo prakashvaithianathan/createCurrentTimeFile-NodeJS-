6b0d938f9aad7a1b7e74a8c552dc9a4b8f732d43
prakashsparkz <indupraka18@gmail.com>
2021-06-12 13:19:56 +0530

    second commit

--- app.js
+++ app.js
@@ -3,18 +3,7 @@ const path = require('path');
 const express=require('express');
 const app= express();
 
-const timeStamp=Date.now();
 
-
-var date = new Date().toDateString();
-
-var dateCheck = new Date();
-
-var hour = dateCheck.getHours();
-var minute = dateCheck.getMinutes();
-var second = dateCheck.getSeconds();
-
-const time = `(${hour}-${minute}-${second})`
 
 
 app.get(`/`,(req, res) =>{
@@ -22,8 +11,21 @@ app.get(`/`,(req, res) =>{
 })
 
 
-  app.get(`/data`,(req, res) =>{
-    fs.open(`${__dirname}/files/${date+time}.txt`, 'w', function (err) {
+  app.get('/data',(req, res) =>{
+
+    const timeStamp=Date.now();
+
+
+    var date = new Date().toDateString();
+    
+    var dateCheck = new Date();
+    var hour = dateCheck.getHours();
+    var minute = dateCheck.getMinutes();
+    var second = dateCheck.getSeconds();
+    
+    const time = `(${hour}-${minute}-${second})`
+
+    fs.open(`${__dirname}/files/${date+time}.txt`,'w+', function (err) {
       if (err) throw err;
       res.send('File is created successfully.');
     });
@@ -33,11 +35,7 @@ app.get(`/`,(req, res) =>{
       
   })
 
- app.get(`/data/open`,(req, res) =>{
-   fs.readdir(__dirname+'/files',(err,data)=>{
-     res.send(data)
-   })
- })
+
   
   app.listen(5000,()=>{
     console.log('server started');
